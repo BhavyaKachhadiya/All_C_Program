1c9a189f9b583205c8d3e289b761243eaf174e3f
BhavyaKachhadiya <kachhadiyabhavya@gmail.com>
2022-09-23 18:02:08 +0530

    added more program

---
+++ sem 1/Practical 3/pr3_1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+// #include <conio.h> for Turbo C++
+
+void main()
+{
+    //Input Part
+    int num; //declaring number
+    // clrscr(); for Turbo C++
+    printf("Enter the number :: "); // Printing 
+    scanf("%d",&num); //taking user value
+
+    //Processing part
+
+    if (num>=0) //conditional applying
+    {
+        printf("This number is Positive.."); // printing this if the value of user is Positive or >0
+    }
+
+    //Output Part
+    printf("\n Number is     :: %d",num); //printing number that user have entered
+    
+    // getch(); for Turbo C++
+}
