c3e688ef53e104e432a1f9708ef40b975a66849e
BhavyaKachhadiya <kachhadiyabhavya@gmail.com>
2022-10-01 12:45:33 +0530

    enhance version of cicle

--- C program/Maths/areas and perimeter/circle.c
+++ C program/Maths/areas and perimeter/circle.c
@@ -1,28 +1,30 @@
 /* Including the standard input/output library. */
 #include <stdio.h>
 // #include <conio.h>
+#define PIE 3.14
 
 /* `void main()` is a function that returns nothing. */
 void main()
 {
     /* Declaring the variables. */
-    float radius,diameter,cf,area;
+    float r,d,cf,area;
 
     // clrscr();
 
     /* Asking the user to enter the radius and then it is storing the value in the variable radius. */
-    printf("Enter the radius :: ");
-    scanf("%f",&radius);
+    printf("Enter the radius of given circle in cm  :: ");
+    scanf("%f",&r);
 
     /* Calculating the diameter, circumference and area of the circle. */
-    diameter = 2*radius;
-    cf = 2*3.14*radius;
-    area = 3.14*radius*radius;
+    d = 2*r;
+    cf = 2*PIE*r;
+    area = PIE*r*r;
 
     /* Printing the values of the variables diameter, cf and area. */
-    printf("Your diameter is :: %f\n",diameter);
-    printf("Your circlecumference  is :: %f\n",cf);
-    printf("Your area  is :: %f\n",area);
+    printf("\nRadius of the given circle is           :: %.2f cm\n", r);
+    printf("Diameter of the given circle is         :: %.2f cm\n",d);
+    printf("Circlecumference of the given circle is :: %.2f cm\n",cf);
+    printf("Area of the given circle is             :: %.2f cm\n",area);
 
     // getch();
 }
