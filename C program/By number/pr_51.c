9ea996fd27bd93cce484bb963821504c2e293854
BhavyaKachhadiya <kachhadiyabhavya@gmail.com>
2022-11-25 16:37:52 +0530

    added 48 to 52

--- C program/By number/pr_51.c
+++ C program/By number/pr_51.c
@@ -1,3 +1,38 @@
 // prime first 10 number
 
+#include <stdio.h>
+
+void main()
+{
+    int i, j,end=30, isPrime; // isPrime is used as flag variable
+
+
+    printf("First prime numbers from 1 to 10 are\n");
+
+    /* Find all Prime numbers between 1 to end */
+    for(i=2; i<=end; i++)
+    {
+        /* Assume that the current number is Prime */
+        isPrime = 1; 
+
+        /* Check if the current number i is prime or not */
+        for(j=2; j<=i/2; j++)
+        {
+            /*
+             * If i is divisible by any number other than 1 and self
+             * then it is not prime number
+             */
+            if(i%j==0)
+            {
+                isPrime = 0;
+            }
+        }
+
+        /* If the number is prime then print */
+        if(isPrime==1)
+        {
+            printf("%d ", i);
+        }
+    }
+}
 // Coming Soon
--- C program/By number/pr_52.c
+++ C program/By number/pr_52.c
@@ -1,4 +1,30 @@
 // 0    1   1   2   3   5   8   13  ... .
 
+#include <stdio.h>
+void main() {
+
+  int i, n1;
+
+  // initialize first and second terms
+  int n2 = 0, n3 = 1;
+
+  // initialize the next term (3rd term)
+  int nextTerm = n2 + n3;
+
+  // get no. of terms from user
+  printf("Enter the number of terms: ");
+  scanf("%d", &n1);
+
+  // print the first two terms n2 and n3
+  printf("Fibonacci Series: %d, %d, ", n2, n3);
+
+  // print 3rd to nth terms
+  for (i = 3; i <= n1; ++i) {
+    printf("%d ", nextTerm);
+    n2 = n3;
+    n3 = nextTerm;
+    nextTerm = n2 + n3;
+  }
+}
 
 // Coming Soon
