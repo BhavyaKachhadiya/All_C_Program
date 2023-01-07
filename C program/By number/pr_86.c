4bd08a52a9f70ef80223976133ce6bcbdb1edd1d
BhavyaKachhadiya <kachhadiyabhavya@gmail.com>
2023-01-07 17:16:41 +0530

    modifing program no 86 and 87

--- C program/By number/pr_86.c
+++ C program/By number/pr_86.c
@@ -2,18 +2,19 @@
 
 int main()
 {
-    int i, n, sum=0;
+    int i=1, n, sum=0;
 
     /* Input range to find sum of odd numbers */
     printf("Enter upper limit: ");
     scanf("%d", &n);
 
     /* Find the sum of all odd number */
-    for(i=1; i<=n; i+=2)
+    while(i<=n)
     {
         sum += i;
+        i+=2;
     }
 
-    printf("Sum of odd numbers = %d", sum);
+    printf("Sum of all odd number between 1 to %d = %d", n, sum);
 
 }
--- C program/By number/pr_87.c
+++ C program/By number/pr_87.c
@@ -2,17 +2,18 @@
 
 int main()
 {
-    int i, n, sum=0;
+    int i=2, n, sum=0;
 
     /* Input upper limit from user */
     printf("Enter upper limit: ");
     scanf("%d", &n);
 
-    for(i=2; i<=n; i+=2)
+    while( i<=n)
     {
         /* Add current even number to sum */
         sum += i;
-    }
+        i+=2;
+    }-
 
     printf("Sum of all even number between 1 to %d = %d", n, sum);
 
